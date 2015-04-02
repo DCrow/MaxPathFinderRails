d42577eb0ec0a1a5bebfb607cd007be1561ae478
d_dcrow <d_dcrow@outlook.com>
2015-04-02 19:21:54 +0300

    Can show max path using rails framework.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,17 @@
-#require_dependency "./lib/path_finder/path_finder.rb"
+require_dependency "./lib/max_path/path_finder.rb"
+require_dependency "./lib/max_path/node_lookup.rb"
+
 class HomeController < ApplicationController
 
 	def index
 	end
 	def calculate
-		n1 = DataChanger.new("out.txt")
-		@res = @n1.parse_data()
+		#@res = params[:q]
+		File.open("./tmp/max_path/in.txt", "w") { |file| file.print params[:numbers]  }
+		d1 = DataChanger.new("./tmp/max_path/in.txt")
+		n1 = Node.new(d1.parse_data_array())
+		n1.find_max_sum_path()
+		d1.write_file("./tmp/max_path/out.txt", n1.get_node_max_path())
+		@res2 = File.read("./tmp/max_path/out.txt")
 	end
 end

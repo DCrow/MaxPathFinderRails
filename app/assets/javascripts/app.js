4a7b47d058f03640897d9518bff4d71b0e1b74c7
d_dcrow <d_dcrow@outlook.com>
2015-04-05 17:33:35 +0300

    added working angular

---
+++ app/assets/javascripts/app.js
@@ -0,0 +1,8 @@
+var pathJs = angular.module('pathJs', []);
+var controllers = angular.module('controllers', []);
+controllers.controller("maxPathController", [
+	'$scope',
+	function($scope) {
+		$scope.mess = "hi";
+	
+}]);
